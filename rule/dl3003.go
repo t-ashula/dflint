c5715fcbea0faf1e9175554c1902f71274e97ee6
t.ashula <dev@ashula.info>
2017-01-16 02:12:38 +0900

    :recycle: use sh for check cd

--- rule/dl3003.go
+++ rule/dl3003.go
@@ -1,9 +1,10 @@
 package rule
 
 import (
-	"regexp"
+	"strings"
 
 	"github.com/docker/docker/builder/dockerfile/parser"
+	"github.com/mvdan/sh/syntax"
 )
 
 func init() {
@@ -15,21 +16,50 @@ func init() {
 	rule.Validate = func(root *parser.Node) bool {
 		valid := true
 		for _, child := range root.Children {
-			if child.Value == "run" {
-				arg := child.Next
-				if arg == nil || arg.Value == "" {
-					continue
-				}
-
-				// TODO: parse shell one liner
-				cd := regexp.MustCompile(`[\s;(|&]*cd[\s)|&]*`)
-				if cd.MatchString(arg.Value) {
-					AppendResult(rule, child)
-					valid = false
-				}
+			if child.Value != "run" {
+				continue
 			}
+
+			arg := child.Next
+			if arg == nil || arg.Value == "" {
+				continue
+			}
+
+			if hasCd(arg.Value) {
+				AppendResult(rule, child)
+				valid = false
+			}
+
 		}
 		return valid
 	}
 	RegisterRule(rule)
+}
+
+func hasCd(script string) bool {
+	ast, err := syntax.Parse(strings.NewReader(script), "", 0)
+	if err != nil {
+		return false
+	}
+	has := false
+	syntax.Walk(ast, func(node syntax.Node) bool {
+		expr, isCall := node.(*syntax.CallExpr)
+		if !isCall {
+			return true
+		}
+
+		fname, err := funcName(expr)
+
+		if err != nil {
+			return false
+		}
+
+		if fname == "cd" {
+			has = true
+			return false
+		}
+
+		return false
+	})
+	return has
 }
