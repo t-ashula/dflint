caf0af3daf22c791ad4c10903b12eed6ff6fe6df
t.ashula <dev@ashula.info>
2017-01-16 01:34:21 +0900

    :recycle: use sh for check pip version pinning

--- rule/dl3013.go
+++ rule/dl3013.go
@@ -2,8 +2,10 @@ package rule
 
 import (
 	"regexp"
+	"strings"
 
 	"github.com/docker/docker/builder/dockerfile/parser"
+	"github.com/mvdan/sh/syntax"
 )
 
 func init() {
@@ -15,8 +17,6 @@ func init() {
 	rule.Validate = func(root *parser.Node) bool {
 		valid := true
 
-		// TODO: parse shell one liner
-		pipPattern := regexp.MustCompile(`\s*pip\s+install\s+.+==.+\s*`)
 		for _, child := range root.Children {
 			if child.Value != "run" {
 				continue
@@ -27,7 +27,8 @@ func init() {
 			}
 
 			cmd := child.Next.Value
-			if !pipPattern.MatchString(cmd) {
+
+			if notPinned(cmd) {
 				valid = false
 				AppendResult(rule, child)
 			}
@@ -36,4 +37,53 @@ func init() {
 		return valid
 	}
 	RegisterRule(rule)
+}
+
+func notPinned(script string) bool {
+	ast, err := syntax.Parse(strings.NewReader(script), "", 0)
+	if err != nil {
+		return false
+	}
+	verPattern := regexp.MustCompile(`.+==.+`)
+	pinned := true
+	syntax.Walk(ast, func(node syntax.Node) bool {
+		expr, isCall := node.(*syntax.CallExpr)
+		if !isCall {
+			return true
+		}
+
+		fname, err := funcName(expr)
+
+		if err != nil || fname != "pip" {
+			return false
+		}
+
+		args, err := funcArgs(expr)
+		if err != nil {
+			return false
+		}
+
+		// TODO: refactor
+		ins, ver := false, true
+		for _, arg := range args {
+			if strings.HasPrefix(arg, "-") {
+				continue
+			}
+			if arg == "install" {
+				ins = true
+				continue
+			}
+			if !verPattern.MatchString(arg) {
+				ver = false
+			}
+			if ins && !ver {
+				pinned = false
+				break
+			}
+		}
+		// no need to digging
+		return false
+	})
+
+	return !pinned
 }
